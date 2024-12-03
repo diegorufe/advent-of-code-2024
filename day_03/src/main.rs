42efe7602e26a2f7d7a9fd2ffacbb3122dad867e
diegorufe <diegorufe@gmail.com>
2024-12-03 19:09:23 +0100

    feat: Day 3 part 2

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -5,39 +5,53 @@ fn read_file(input_file_name: &str) -> String {
     return fs::read_to_string(input_file_name).expect("Unable to read file");
 }
 
-fn parse(data: String) -> i32 {
+fn parse(data: String, check_operation_enabled: bool) -> i32 {
+    let do_operation_key: &str = "do()";
+    let dont_operation_key: &str = "don't()";
+
     let mut result: i32 = 0;
-    let mut builder = String::new();
+    let mut builder_operation = String::new();
     let mut builder_multiplicand = String::new();
     let mut builder_multiplier = String::new();
+    let mut builder = String::new();
+    let mut enabled_operation: bool = true;
 
     for character in data.chars() {
+        builder.push_str(character.to_string().as_str());
+
+        if check_operation_enabled
+            && (builder.contains(do_operation_key) || builder.contains(dont_operation_key))
+        {
+            enabled_operation = builder.contains(do_operation_key);
+            builder.clear();
+        }
+
         match character {
             'm' => {
-                resolve_character_in_builer(&mut builder, character, 0);
+                resolve_character_in_builder(&mut builder_operation, character, 0);
             }
             'u' => {
-                resolve_character_in_builer(&mut builder, character, 1);
+                resolve_character_in_builder(&mut builder_operation, character, 1);
             }
             'l' => {
-                resolve_character_in_builer(&mut builder, character, 2);
+                resolve_character_in_builder(&mut builder_operation, character, 2);
             }
             '(' => {
-                resolve_character_in_builer(&mut builder, character, 3);
+                resolve_character_in_builder(&mut builder_operation, character, 3);
             }
             ',' => {
-                if builder.len() == 4 {
-                    builder.push_str(character.to_string().as_str());
+                if builder_operation.len() == 4 {
+                    builder_operation.push_str(character.to_string().as_str());
                 } else {
                     clear_builders(
-                        &mut builder,
+                        &mut builder_operation,
                         &mut builder_multiplicand,
                         &mut builder_multiplier,
                     );
                 }
             }
             ')' => {
-                if builder.len() == 5 {
+                if builder_operation.len() == 5 && enabled_operation {
                     let multiplicand: i32 = builder_multiplicand
                         .parse()
                         .expect("Error on parsing multiplicand");
@@ -47,20 +61,21 @@ fn parse(data: String) -> i32 {
                     let product: i32 = multiplicand * multiplier;
                     result = result + product;
                 }
+
                 clear_builders(
-                    &mut builder,
+                    &mut builder_operation,
                     &mut builder_multiplicand,
                     &mut builder_multiplier,
                 );
             }
             _ => {
-                if character.is_digit(10) && builder.len() == 4 {
+                if character.is_digit(10) && builder_operation.len() == 4 {
                     builder_multiplicand.push_str(character.to_string().as_str());
-                } else if character.is_digit(10) && builder.len() == 5 {
+                } else if character.is_digit(10) && builder_operation.len() == 5 {
                     builder_multiplier.push_str(character.to_string().as_str());
                 } else {
                     clear_builders(
-                        &mut builder,
+                        &mut builder_operation,
                         &mut builder_multiplicand,
                         &mut builder_multiplier,
                     );
@@ -72,7 +87,7 @@ fn parse(data: String) -> i32 {
     return result;
 }
 
-fn resolve_character_in_builer(builder: &mut String, character: char, length: i32) {
+fn resolve_character_in_builder(builder: &mut String, character: char, length: i32) {
     if builder.len() == length.try_into().unwrap() {
         builder.push_str(character.to_string().as_str());
     } else {
@@ -93,7 +108,7 @@ fn clear_builders(
 fn history_example_part_one() {
     let start = Instant::now();
     let data: String = read_file("./src/history_example_part_one_data.txt");
-    let result = parse(data);
+    let result = parse(data, false);
     let duration = start.elapsed();
     print!(
         "History example part one. Result {}. Time: {:?} \n",
@@ -104,7 +119,7 @@ fn history_example_part_one() {
 fn history_part_one() {
     let start = Instant::now();
     let data = read_file("./src/history_part_one_data.txt");
-    let result = parse(data);
+    let result = parse(data, false);
     let duration = start.elapsed();
     print!(
         "History part one. Result {}. Time: {:?} \n",
@@ -112,7 +127,31 @@ fn history_part_one() {
     )
 }
 
+fn history_example_part_two() {
+    let start = Instant::now();
+    let data: String = read_file("./src/history_example_part_two_data.txt");
+    let result = parse(data, true);
+    let duration = start.elapsed();
+    print!(
+        "History example part one. Result {}. Time: {:?} \n",
+        result, duration
+    )
+}
+
+fn history_part_two() {
+    let start = Instant::now();
+    let data: String = read_file("./src/history_part_two_data.txt");
+    let result = parse(data, true);
+    let duration = start.elapsed();
+    print!(
+        "History example part one. Result {}. Time: {:?} \n",
+        result, duration
+    )
+}
+
 fn main() {
     history_example_part_one();
     history_part_one();
+    history_example_part_two();
+    history_part_two();
 }
