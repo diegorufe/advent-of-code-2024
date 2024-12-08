e9108860e1be69b0e995804111db5aea044a8c30
diegorufe <diegorufe@gmail.com>
2024-12-08 15:12:10 +0100

    Day 8 part 2

--- day_08/src/main.rs
+++ day_08/src/main.rs
@@ -61,9 +61,76 @@ impl Antenna {
         return Coordinate::new(y, x, first.max_y, first.max_x);
     }
 
-    fn calculate_antinodes(&self) -> HashSet<Coordinate> {
+    fn calculate_t_antinodes_backguard(
+        &self,
+        first: Coordinate,
+        second: Coordinate,
+    ) -> Vec<Coordinate> {
+        if second.y <= first.y {
+            return Vec::new();
+        }
+
+        let mut values: Vec<Coordinate> = Vec::new();
+
+        let mut index_calculate = first.y;
+
+        let y_diff = (second.y - first.y) * -1;
+        let x_diff = (second.x - first.x) * -1;
+
+        let mut y = first.y;
+        let mut x = first.x;
+
+        while index_calculate > 0 {
+            y += y_diff;
+            x += x_diff;
+
+            values.push(Coordinate::new(y, x, first.max_y, first.max_x));
+
+            index_calculate = y;
+        }
+
+        return values;
+    }
+
+    fn calculate_t_antinodes_forward(
+        &self,
+        first: Coordinate,
+        second: Coordinate,
+    ) -> Vec<Coordinate> {
+        if first.y >= second.y {
+            return Vec::new();
+        }
+
+        let size = first.max_y as isize;
+        let mut values: Vec<Coordinate> = Vec::new();
+
+        let mut index_calculate = first.y;
+
+        let y_diff = second.y - first.y;
+        let x_diff = second.x - first.x;
+
+        let mut y = first.y;
+        let mut x = first.x;
+
+        while index_calculate < size {
+            y += y_diff;
+            x += x_diff;
+
+            values.push(Coordinate::new(y, x, first.max_y, first.max_x));
+
+            index_calculate = y;
+        }
+
+        return values;
+    }
+
+    fn calculate_antinodes(
+        &self,
+        calculate_t_antinodes: bool,
+    ) -> (HashSet<Coordinate>, HashSet<Coordinate>) {
         let mut calculated_antinodes: HashSet<Coordinate> = HashSet::new();
         let mut unique_coordinates: HashSet<Coordinate> = HashSet::new();
+        let mut calculated_t_antinodes: HashSet<Coordinate> = HashSet::new();
 
         for coordinate in self.coordinates.iter() {
             unique_coordinates.insert(*coordinate);
@@ -73,24 +140,41 @@ impl Antenna {
             let mut next = 0;
 
             while next < self.coordinates.len() {
+                let first = coordinate.clone();
+                let second = self.coordinates[next].clone();
+
+                if calculate_t_antinodes {
+                    let t_backguard_values: Vec<Coordinate> =
+                        self.calculate_t_antinodes_backguard(first, second);
+
+                    for t_backguard in t_backguard_values {
+                        if t_backguard.is_valid() {
+                            calculated_t_antinodes.insert(t_backguard);
+                        }
+                    }
+
+                    let t_forward_values: Vec<Coordinate> =
+                        self.calculate_t_antinodes_forward(first, second);
+
+                    for t_forward in t_forward_values {
+                        if t_forward.is_valid() {
+                            calculated_t_antinodes.insert(t_forward);
+                        }
+                    }
+                }
+
                 if index == next {
                     next += 1;
                     continue;
                 }
 
-                let backguard = self.calculate_antinodes_backguard(
-                    coordinate.clone(),
-                    self.coordinates[next].clone(),
-                );
+                let backguard = self.calculate_antinodes_backguard(first, second);
 
                 if !unique_coordinates.contains(&backguard) && backguard.is_valid() {
                     calculated_antinodes.insert(backguard);
                 }
 
-                let forward = self.calculate_antinodes_forward(
-                    coordinate.clone(),
-                    self.coordinates[next].clone(),
-                );
+                let forward = self.calculate_antinodes_forward(first, second);
 
                 if !unique_coordinates.contains(&forward) && forward.is_valid() {
                     calculated_antinodes.insert(forward);
@@ -100,7 +184,7 @@ impl Antenna {
             }
         }
 
-        return calculated_antinodes;
+        return (calculated_antinodes, calculated_t_antinodes);
     }
 }
 
@@ -139,14 +223,25 @@ fn read_data(input_file_name: &str) -> Vec<Antenna> {
     return antennas;
 }
 
-fn count_antinodes(antennas: Vec<Antenna>) -> usize {
+fn count_antinodes(antennas: Vec<Antenna>, calculate_t_antinodes: bool) -> usize {
     let mut calculated_antinodes: HashSet<Coordinate> = HashSet::new();
 
     for antenna in antennas {
-        let calculated_antinodes_antenna = antenna.calculate_antinodes();
+        let (calculated_antinodes_antenna, calculated_t_antinodes_antenna) =
+            antenna.calculate_antinodes(calculate_t_antinodes);
 
         for coordinate in calculated_antinodes_antenna {
             calculated_antinodes.insert(coordinate);
+        }
+
+        for coordinate in calculated_t_antinodes_antenna {
+            calculated_antinodes.insert(coordinate);
+        }
+
+        if calculate_t_antinodes {
+            for coordinate in antenna.coordinates {
+                calculated_antinodes.insert(coordinate);
+            }
         }
     }
 
@@ -156,7 +251,7 @@ fn count_antinodes(antennas: Vec<Antenna>) -> usize {
 fn history_example_part_one() {
     let start = Instant::now();
     let anntenas: Vec<Antenna> = read_data("./src/history_example_part_one_data.txt");
-    let count_antinodes = count_antinodes(anntenas);
+    let count_antinodes = count_antinodes(anntenas, false);
     let duration = start.elapsed();
     print!(
         "History example part one. Count antinodes {}. Time: {:?} \n",
@@ -167,7 +262,7 @@ fn history_example_part_one() {
 fn history_part_one() {
     let start = Instant::now();
     let anntenas: Vec<Antenna> = read_data("./src/history_part_one_data.txt");
-    let count_antinodes = count_antinodes(anntenas);
+    let count_antinodes = count_antinodes(anntenas, false);
     let duration = start.elapsed();
     print!(
         "History part one. Count antinodes {}. Time: {:?} \n",
@@ -175,7 +270,31 @@ fn history_part_one() {
     )
 }
 
+fn history_example_part_two() {
+    let start = Instant::now();
+    let anntenas: Vec<Antenna> = read_data("./src/history_example_part_two_data.txt");
+    let count_antinodes = count_antinodes(anntenas, true);
+    let duration = start.elapsed();
+    print!(
+        "History example part two. Count antinodes {}. Time: {:?} \n",
+        count_antinodes, duration
+    )
+}
+
+fn history_part_two() {
+    let start = Instant::now();
+    let anntenas: Vec<Antenna> = read_data("./src/history_part_two_data.txt");
+    let count_antinodes = count_antinodes(anntenas, true);
+    let duration = start.elapsed();
+    print!(
+        "History part two. Count antinodes {}. Time: {:?} \n",
+        count_antinodes, duration
+    )
+}
+
 fn main() {
     history_example_part_one();
     history_part_one();
+    history_example_part_two();
+    history_part_two();
 }
